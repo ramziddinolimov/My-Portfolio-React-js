a937d8f14a051ba496cb9050f830afc0ba9133de
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-14 23:01:35 +0500

    background

--- my-app/src/styles/GlobalStyle.js
+++ my-app/src/styles/GlobalStyle.js
@@ -4,6 +4,14 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     :root{
         --primary-color: #007bff;
+        --secondary-color: #6c757d;
+        --background-dark-color: #10121A;
+        --border-color: #2e344e;
+        --background-light-color: #f1f1f1;
+        --white-color: #fff;
+        --font-light-color: #a4acc4;
+        --font-dark-color: #313131;
+        --font-dark-color-2: #151515;
     }
     
     *{
@@ -15,7 +23,7 @@ const GlobalStyle = createGlobalStyle`
     }
 
     body{
-        background-color: var(--background-color);
+        background-color: var(--background-dark-color);
     }
 `;
 
