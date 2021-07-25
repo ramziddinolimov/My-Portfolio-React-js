6c945a967f0b46b614dec257f3a1845ad1f76a79
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-26 01:47:27 +0500

    dasdasd

--- my-app/src/Components/Title.js
+++ my-app/src/Components/Title.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 function Title({title, span}) {
     return (
         <TitleStyled>
-            <h2>{title} <span>{span}</span></h2>
+            <h2>{title} <b><span>{span}</span></b></h2>
         </TitleStyled>
     )
 }
@@ -16,10 +16,14 @@ const TitleStyled = styled.div`
         font-size: 3.8rem;
         font-weight: 600;
         text-transform: uppercase;
+        position: relative;
         span{
             font-weight: 900;
             color: rgba(25,29,43, .44);
             font-size: 5rem;
+            position: absolute;
+            left: 0%;
+            top: 10%;
         }
     }
 `;
