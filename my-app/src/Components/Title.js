91b9598ebb716f714609b050eb424737567b11cd
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-26 02:17:16 +0500

    image section started

--- my-app/src/Components/Title.js
+++ my-app/src/Components/Title.js
@@ -13,17 +13,41 @@ const TitleStyled = styled.div`
     position: relative;
     h2{
         color: var(--white-color);
-        font-size: 3.8rem;
+        font-size: 3.1rem;
         font-weight: 600;
         text-transform: uppercase;
         position: relative;
+        padding-bottom: .7rem;
+        &::before{
+            content: "";
+            position: absolute;
+            bottom: 0;
+            width: 7.4rem;
+            height: .33rem;
+            background-color: var(--background-light-color-2);
+            border-radius: 15px;
+            left: 0;
+        }
+
+        &::after{
+            content: "";
+            position: absolute;
+            bottom: 0;
+            width: 3.5rem;
+            height: .33rem;
+            background-color: var(--primary-color);
+            border-radius: 15px;
+            left: 0;
+            
+        }
         span{
             font-weight: 900;
             color: rgba(25,29,43, .44);
             font-size: 5rem;
             position: absolute;
             left: 0%;
-            top: 10%;
+            top: 30%;
+            z-index: -1;
         }
     }
 `;
