b7e0e1b00153c8650aac7b056a4d1b0c464b01dc
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-28 00:02:09 +0500

    reviwv  section continued

--- my-app/src/Components/ReviewItem.js
+++ my-app/src/Components/ReviewItem.js
@@ -13,6 +13,17 @@ const RevivewItemStyled = styled.div`
     height: 15vh;
     padding: 2rem 1rem;
     border-left: 6px solid var(--border-color);
+    background-color: var(--background-dark-grey);
+    position: relative;
+    &::after{
+        content: "";
+        position: absolute;
+        left: 1rem;
+        border-width: .8rem;
+        top: 100%;
+        border-style: solid;
+        border-color: var(--background-dark-grey) transparent transparent var(--background-dark-grey); 
+    }
     p{
         padding: 1rem 0;
     }
