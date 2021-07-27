455f518395250a897b603ba9ae8b2224076f06d0
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-27 14:10:38 +0500

    review  item started

--- my-app/src/Components/ReviewItem.js
+++ my-app/src/Components/ReviewItem.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
 
-function ReviewItem() {
+function ReviewItem({text}) {
     return (
-        <div>
-            
-        </div>
+        <RevivewItemStyled>
+            <p>{text}</p>
+        </RevivewItemStyled>
     )
 }
 
-export default ReviewItem
+const RevivewItemStyled = styled.div`
+    height: 15vh;
+    padding: 2rem 1rem;
+    border-left: 6px solid var(--border-color);
+    p{
+        padding: 1rem 0;
+    }
+`;
+
+export default ReviewItem;
--- my-app/src/Components/ReviewsSetion.js
+++ my-app/src/Components/ReviewsSetion.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {InnerLayout} from '../styles/Layouts';
 import Title from '../Components/Title';
+import ReviewItem from '../Components/ReviewItem';
 
 function ReviewsSetion() {
     return (
@@ -9,7 +10,9 @@ function ReviewsSetion() {
             <ReviewsStyled>
                 <Title title={'Reviews'} span={'Reviews'} />
                 <InnerLayout>
-                    
+                    <div className="reviews">
+                        <ReviewItem text={'fewfwe fewfwe fwefw fwef wefwefw efwef wefwefwe fwe fwe'} />
+                    </div>
                 </InnerLayout>
             </ReviewsStyled>
         </InnerLayout>
