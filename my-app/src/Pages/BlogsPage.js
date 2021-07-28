c146b5eee3f85e13a911d3fdb749cc5ce553fb10
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-29 02:06:59 +0500

    blogs javascript continued

--- my-app/src/Pages/BlogsPage.js
+++ my-app/src/Pages/BlogsPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import Title from '../Components/Title'
 import blogs from '../data/blogs';
 
 function BlogsPage() {
     return (
         <BlogsStyled>
+            <Title title={'Blogs'} span={'Blogs'} />
             {
                 blogs.map((blog)=>{
                     return <div key={blog.id}>
