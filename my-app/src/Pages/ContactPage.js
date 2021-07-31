51a115366bc92286a95cc2385873cfe695c62eb8
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-01 01:25:14 +0500

    contact page continued

--- my-app/src/Pages/ContactPage.js
+++ my-app/src/Pages/ContactPage.js
@@ -2,13 +2,18 @@ import React from 'react';
 import styled from 'styled-components';
 import {MainLayout, InnerLayout} from '../styles/Layouts';
 import Title from '../Components/Title';
+import PrimaryButton from '../Components/PrimaryButton';
 
 function ContactPage() {
     return (
         <MainLayout>
             <Title title={'Contact'} span={'Contact'} />
+            <ContactPageStyled>
             <InnerLayout className={'contact-section'}>
                 <div className="left-content">
+                    <div className="contact-title">
+                        <h4>Get In Touch</h4>
+                    </div>
                     <form className="form">
                         <div className="form-field">
                             <label htmlFor="name">Enter you name*</label>
@@ -24,20 +29,68 @@ function ContactPage() {
                             <label htmlFor="subject">Enter you subject*</label>
                             <input type="text" id="subject" />
                         </div>
-                        <div className="text-area">
+                        <div className="form-field">
                             <label htmlFor="">Enter your message*</label>
                             <textarea name="textarea" id="textarea" cols="30" rows="10"></textarea>
+                        </div>
+                        <div className="form-field">
+                            <PrimaryButton title={'Send email'} />
                         </div>
                     </form>
                 </div>
                 <div className="right-content"></div>
             </InnerLayout>
+            </ContactPageStyled>
         </MainLayout>
     )
 }
 
 const ContactPageStyled = styled.section`
-
+    .contact-section{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        .contact-title{
+            h4{
+                color: var(--white-color);
+                padding: 1rem 0;
+                font-size: 1.8rem;
+            }
+        }
+        .form{
+            width: 100%;
+            .form-field{
+                margin-top: 2rem;
+                position: relative;
+                width: 100%;
+                label{
+                    position: absolute;
+                    left: 20px;
+                    top: -25px;
+                    display: inline-block;
+                    background-color: var(--background-dark-color);
+                
+                    color: inherit;
+                }
+                input{
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    background: transparent;
+                    height: 60px;
+                    padding: 0 15px;
+                    width: 100%;
+                    color: inherit;
+                }
+                textarea{
+                    background-color: transparent;
+                    border: 1px solid var(--border-color);
+                    outline: none;
+                    color: inherit;
+                    width: 100%;
+                    padding: .8rem 1rem;
+                }
+            }
+        }
+    }
 `;
 
 export default ContactPage;
