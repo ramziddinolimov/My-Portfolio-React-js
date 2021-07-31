a1e4025f0172aa98c082eb6333803ad5547d42f4
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-07-31 23:58:54 +0500

    ececfre

--- my-app/src/Pages/ContactPage.js
+++ my-app/src/Pages/ContactPage.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {MainLayout, InnerLayout} from '../styles/Layouts';
+import Title from '../Components/Title';
 
 function ContactPage() {
     return (
-        <div>
-            
-        </div>
+        <MainLayout>
+            <Title title={'Contact'} span={'Contact'} />
+            <InnerLayout>
+                
+            </InnerLayout>
+        </MainLayout>
     )
 }
 
-export default ContactPage
+const ContactPageStyled = styled.section`
+
+`;
+
+export default ContactPage;
