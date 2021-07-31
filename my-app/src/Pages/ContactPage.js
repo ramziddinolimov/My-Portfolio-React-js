853d9a93f9bd5fcff98bf0cd520f27b8e667f9d7
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-01 00:50:33 +0500

    Contact page started

--- my-app/src/Pages/ContactPage.js
+++ my-app/src/Pages/ContactPage.js
@@ -7,8 +7,30 @@ function ContactPage() {
     return (
         <MainLayout>
             <Title title={'Contact'} span={'Contact'} />
-            <InnerLayout>
-                
+            <InnerLayout className={'contact-section'}>
+                <div className="left-content">
+                    <form className="form">
+                        <div className="form-field">
+                            <label htmlFor="name">Enter you name*</label>
+                            <input type="text" id="name" />
+                        </div>
+
+                        <div className="form-field">
+                            <label htmlFor="email">Enter you email*</label>
+                            <input type="email" id="email" />
+                        </div>
+
+                        <div className="form-field">
+                            <label htmlFor="subject">Enter you subject*</label>
+                            <input type="text" id="subject" />
+                        </div>
+                        <div className="text-area">
+                            <label htmlFor="">Enter your message*</label>
+                            <textarea name="textarea" id="textarea" cols="30" rows="10"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div className="right-content"></div>
             </InnerLayout>
         </MainLayout>
     )
