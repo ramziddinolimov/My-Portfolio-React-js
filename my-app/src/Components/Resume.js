ad78f9fea486b51408a217f146e5a20dc70b7a3a
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-02 00:47:21 +0500

    small section continued

--- my-app/src/Components/Resume.js
+++ my-app/src/Components/Resume.js
@@ -2,14 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import {MainLayout, InnarLayout, InnerLayout} from '../styles/Layouts'
 import Title from '../Components/Title'
+import SmallTitle from '../Components/SmallTitle';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import SchoolIcon from '@material-ui/icons/School';
 
 function Resume() {
+    const briefcase = <BusinessCenterIcon />
+    const school = <SchoolIcon />
     return (
         
             <ResumeStyled>
                 <Title title={'Resume'} span={'resume'} />
                 <InnerLayout>
-
+                    <SmallTitle icon={briefcase} title={'Working Experience'} />
+                    <div className="resume-content"></div>
                 </InnerLayout>
             </ResumeStyled>
         
--- my-app/src/Components/SmallTitle.js
+++ my-app/src/Components/SmallTitle.js
@@ -11,7 +11,18 @@ function SmallTitle({icon, title}) {
 }
 
 const SmallTitleStyled = styled.div`
-
+    display: flex;
+    align-items: center;
+    p{
+        padding-right: 1rem;
+        svg{
+            font-size: 3rem;
+        }
+    }
+    h3{
+        color: var(--white-color);
+        font-size: 2rem;
+    }
 `;
 
 export default SmallTitle;
