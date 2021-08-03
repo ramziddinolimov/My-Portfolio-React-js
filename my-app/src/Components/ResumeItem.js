c85317e665b976243fde5457db4776c05c975533
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-03 22:56:31 +0500

    resume item started

---
+++ my-app/src/Components/ResumeItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function ResumeItem() {
+    return (
+        <ResumeItemStyled>
+            <div className="left-content"></div>
+            <div className="right-content"></div>
+        </ResumeItemStyled>
+    )
+}
+
+const ResumeItemStyled = styled.div`
+
+`;
+
+export default ResumeItem;
