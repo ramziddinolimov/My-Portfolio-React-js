01f3d8ef8268c4259720ff04a9d396365991f6b1
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-05 02:15:16 +0500

    button started

--- my-app/src/Components/Button.js
+++ my-app/src/Components/Button.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Buuton({filter, button}) {
+function Button({filter, button}) {
     return (
-        <div>
+        <ButtonsStyled>
             {
                 button.map((but, i) =>{
-                    return <button key={i}>
+                    return <ButtonStyled key={i} onClick={() => filter(but)}>
                         {but}
-                    </button>
+                    </ButtonStyled>
                 })
             }
             
-        </div>
+        </ButtonsStyled>
     )
 }
 
-export default Buuton;
+const ButtonStyled = styled.button`
+    outline: none;
+    border: none;
+    background-color: var(--background-light-color-2);
+    padding: .5rem 2rem;  
+    font-size: inherit;
+    color: var(--white-color);
+    &:not(:last-child){
+        margin-right: 1rem;
+    }
+`;
+
+const ButtonsStyled = styled.div`
+    display: flex;  
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 70%;
+    margin: 2.4rem auto;
+`;
+
+export default Button;
--- my-app/src/Pages/PortfoliosPage.js
+++ my-app/src/Pages/PortfoliosPage.js
@@ -5,13 +5,18 @@ import portfolios from '../data/portfolios';
 import Menu from '../Components/Menu';
 import Button from '../Components/Button';
 
-const allButtons = ['All', 'Animation', ...portfolios.map(item => item.category)]
+const allButtons = ['All', ...new Set(portfolios.map(item => item.category))]
 
 function PortfoliosPage() {
     const [menuItem, setMenuItems] = useState(portfolios);
     const [button, setButtons] = useState(allButtons);
 
     const filter = (button) => {
+
+        if(button === 'All'){
+            setMenuItems(portfolios);   
+            return;     
+        }
 
         const filteredData = portfolios.filter(item => item.category === button);
         setMenuItems(filteredData);
