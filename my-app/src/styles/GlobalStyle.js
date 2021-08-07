195a9a9b6099b08bd64cdd4e995d0aac2b44979f
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-07 23:41:29 +0500

    color continued

--- my-app/src/styles/GlobalStyle.js
+++ my-app/src/styles/GlobalStyle.js
@@ -23,19 +23,19 @@ const GlobalStyle = createGlobalStyle`
     }
 
     .light-theme{
-        --primary-color: #151515;
+        --primary-color: #007bff;
         --primary-color-light: #057FFF;
         --secondary-color: #6c757d;
-        --background-dark-color: #10121A;
+        --background-dark-color: #F1F1F1;
         --background-dark-grey: #191D2B;
-        --border-color: #2e344e;
+        --border-color: #cbced8;
         --background-light-color: #f1f1f1;
         --background-light-color-2: rgba(3,127,255, .3);
-        --white-color: #fff;
+        --white-color: #151515;
         --font-light-color: #313131;
         --font-dark-color: #313131;
         --font-dark-color-2: #151515;
-        --sidebar-dark-color: #191D2B;
+        --sidebar-dark-color: #E4E4E4;
         --scrollbar-bg-color: #383838;
         --scrollbar-thump-color: #6b6b6b;
         --scrollbar-track-color: #383838;
